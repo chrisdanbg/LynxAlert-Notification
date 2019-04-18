8727c79286e978101492601164032a8a2003a346
Kristiyan <kristiyan@Kristiyans-MacBook-Pro.local>
2019-04-18 21:29:59 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Really simple Javascript alert I've built for personal purposes.
+
+![](https://media.giphy.com/media/YBJBvBJPQcTguxwdC8/giphy.gif)
+
+# USAGE
+
+Don't forget to import the style and script files into your project.
+ <link rel="stylesheet" href="lynxalert.css">
+<script src="lynxalert.js"></script>
+
+Then call:
+
+```javascript
+// color = currently only 'green' and 'red' colors.
+// msg = Your message.
+lynxAlert(color,msg);
+```
---
+++ lynxAlert.js
@@ -0,0 +1,35 @@
+﻿function lynxAlert(color,msg) {
+    if (document.getElementById('nt-bdy')) {
+        var element = document.getElementById('nt-bdy');
+        element.parentNode.removeChild(element);
+    }
+        var notification = document.createElement("div");
+        notification.classList.add('notification-body');
+        notification.setAttribute("id", "nt-bdy");
+
+        var notificationmsg = document.createElement("h5");
+        notificationmsg.classList.add('notification-msg');
+        notificationmsg.setAttribute("id", "nt-msg");
+
+        notification.appendChild(notificationmsg);
+        document.body.appendChild(notification);
+
+        notification.classList.remove('goBack');
+        notification.classList.add('animated');
+
+        if (color === 'green') {
+            notification.classList.add('green');
+        } 
+        else if (color === 'red')
+        {
+            notification.classList.add('red');
+        }
+        notificationmsg.innerHTML = msg;
+        setTimeout(function () { animBack(); }, 2000);
+    }
+
+    function animBack() {
+        var notification = document.getElementById('nt-bdy');
+        notification.classList.remove('animated');
+        notification.classList.add('goBack');
+    }
